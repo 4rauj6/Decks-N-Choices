09cef211fb1b24afb53c840f3b5f04df6d58fc76
João Victor <159325010+4rauj6@users.noreply.github.com>
2025-09-02 16:33:34 -0300

    Add files via upload

--- cartas.js
+++ cartas.js
@@ -1,9 +1,19 @@
-document.addEventListener("keydown", function(event){
-    if(event.code === "Space"){
-        event.preventDefault();
+document.addEventListener("keydown", function(event) {
+    if (event.code === "Space") {
+       
         document.querySelector(".telaInicial").style.display = "none";
-        document.querySelector(".click").style.display = "none";
         document.querySelector(".telaJogo").style.display = "block";
-        document.querySelector(".img").style.display = "block";
+
+        
+        document.body.classList.add("bgJogo");
     }
 });
+
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById("emailUser");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+}
